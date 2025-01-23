3a8c9ab01d4cdcf34f0a427f919325afb5b9418d
cloudi32003 <cloudi32003@gmail.com>
2025-01-23 21:27:06 +0800

    config File v5

--- src/main/java/sg/edu/ntu/split_and_share/controller/DashboardController.java
+++ src/main/java/sg/edu/ntu/split_and_share/controller/DashboardController.java
@@ -1,98 +1,85 @@
 package sg.edu.ntu.split_and_share.controller;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-
-<<<<<<< HEAD
 // import java.math.BigDecimal;
 // import java.util.List;
 // import java.util.Map;
-=======
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
->>>>>>> 081c5cfa0654dfca65ba31d43b2eb6a236b7aaeb
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+// import java.math.BigDecimal;
+// import java.util.List;
+// import java.util.Map;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.RestController;
-
-=======
->>>>>>> 081c5cfa0654dfca65ba31d43b2eb6a236b7aaeb
-import sg.edu.ntu.split_and_share.entity.Expense;
+// import sg.edu.ntu.split_and_share.entity.Expense;
 import sg.edu.ntu.split_and_share.service.DashboardService;
 
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController {
 
-  private final DashboardService dashboardService;
+  // private final DashboardService dashboardService;
 
-  public DashboardController(DashboardService dashboardService) {
-    this.dashboardService = dashboardService;
-  }
+  // public DashboardController(DashboardService dashboardService) {
+  //   this.dashboardService = dashboardService;
+  // }
 
   // Get total sum of all expenses
   // http://localhost:8080/api/dashboard/{username}/total-sum
-  @GetMapping("/{username}/total-sum")
-  public ResponseEntity<Double> calculateTotalSum(@PathVariable String username) {
-    double totalSum = dashboardService.calculateTotalSum(username);
-    return new ResponseEntity<>(totalSum, HttpStatus.OK);
-  }
+  // @GetMapping("/{username}/total-sum")
+  // public ResponseEntity<Double> calculateTotalSum(@PathVariable String username) {
+  //   double totalSum = dashboardService.calculateTotalSum(username);
+  //   return new ResponseEntity<>(totalSum, HttpStatus.OK);
+  // }
 
   // Get total sum of each expense type
   // http://localhost:8080/api/dashboard/{username}/sum-by-type
-  @GetMapping("/{username}/sum-by-type")
-  public ResponseEntity<Map<String, Double>> sumExpensesByType(@PathVariable String username) {
-    Map<String, Double> expensesByType = dashboardService.sumExpensesByType(username);
-    return new ResponseEntity<>(expensesByType, HttpStatus.OK);
-  }
+  // @GetMapping("/{username}/sum-by-type")
+  // public ResponseEntity<Map<String, Double>> sumExpensesByType(@PathVariable String username) {
+  //   Map<String, Double> expensesByType = dashboardService.sumExpensesByType(username);
+  //   return new ResponseEntity<>(expensesByType, HttpStatus.OK);
+  // }
 
   // Count the number of each expense type
   // http://localhost:8080/api/dashboard/{username}/count-by-type
-  @GetMapping("/{username}/count-by-type")
-  public ResponseEntity<Map<String, Long>> countExpensesByType(@PathVariable String username) {
-    Map<String, Long> expenseCountByType = dashboardService.countExpensesByType(username);
-    return ResponseEntity.ok(expenseCountByType);
-  }
+  // @GetMapping("/{username}/count-by-type")
+  // public ResponseEntity<Map<String, Long>> countExpensesByType(@PathVariable String username) {
+  //   Map<String, Long> expenseCountByType = dashboardService.countExpensesByType(username);
+  //   return ResponseEntity.ok(expenseCountByType);
+  // }
 
   // Count the grand total number of expenses
   // http://localhost:8080/api/dashboard/{username}/count-total
-  @GetMapping("/{username}/count-total")
-  public ResponseEntity<Long> countTotalNumberOfExpenses(@PathVariable String username) {
-    long totalNumberOfExpenses = dashboardService.countTotalNumberOfExpenses(username);
-    return new ResponseEntity<>(totalNumberOfExpenses, HttpStatus.OK);
-  }
+  // @GetMapping("/{username}/count-total")
+  // public ResponseEntity<Long> countTotalNumberOfExpenses(@PathVariable String username) {
+  //   long totalNumberOfExpenses = dashboardService.countTotalNumberOfExpenses(username);
+  //   return new ResponseEntity<>(totalNumberOfExpenses, HttpStatus.OK);
+  // }
 
   // Calculate individual/group member balances
   // http://localhost:8080/api/dashboard/{username}/balances
-  @GetMapping("/{username}/balances")
-  public ResponseEntity<Map<String, BigDecimal>> calculateNetBalances(@PathVariable String username) {
-    Map<String, BigDecimal> balances = dashboardService.calculateNetBalances(username);
-    return new ResponseEntity<>(balances, HttpStatus.OK);
-  }
+  // @GetMapping("/{username}/balances")
+  // public ResponseEntity<Map<String, BigDecimal>> calculateNetBalances(@PathVariable String username) {
+  //   Map<String, BigDecimal> balances = dashboardService.calculateNetBalances(username);
+  //   return new ResponseEntity<>(balances, HttpStatus.OK);
+  // }
 
   // Settle balance among group members
   // http://localhost:8080/api/dashboard/{username}settlement
-  @GetMapping("/{username}/settlement")
-  public ResponseEntity<Map<String, List<String>>> SettleBalances(@PathVariable String username) {
-    Map<String, List<String>> settlement = dashboardService.settleBalances(username);
-    return new ResponseEntity<>(settlement, HttpStatus.OK);
-  }
+  // @GetMapping("/{username}/settlement")
+  // public ResponseEntity<Map<String, List<String>>> SettleBalances(@PathVariable String username) {
+  //   Map<String, List<String>> settlement = dashboardService.settleBalances(username);
+  //   return new ResponseEntity<>(settlement, HttpStatus.OK);
+  // }
 
   // Fetch all expenses details
   // http://localhost:8080/api/dashboard/{username}/expenses
-  @GetMapping("/{username}/expenses")
-  public ResponseEntity<List<Expense>> getAllIndividualExpenses(@PathVariable String username) {
-    List<Expense> expenses = dashboardService.getAllIndividualExpenses(username);
-    return new ResponseEntity<>(expenses, HttpStatus.OK);
-  }
+  // @GetMapping("/{username}/expenses")
+  // public ResponseEntity<List<Expense>> getAllIndividualExpenses(@PathVariable String username) {
+  //   List<Expense> expenses = dashboardService.getAllIndividualExpenses(username);
+  //   return new ResponseEntity<>(expenses, HttpStatus.OK);
+  // }
 
   // Reset dashboard
   // http://localhost:8080/api/dashboard/{username}/reset
